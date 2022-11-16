bfae93106c463da3f9f42b0e78ef4e939bcb2437
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-16 14:11:07 +0000

    [Finish]deviseのルーティング(customers, admin)の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # デフォルトのルーティングは削除(参考：https://nllllll.com/ruby-on-rails/rails-devise/)
+  # devise_for :admins
+  # devise_for :customers
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  # 管理者用
+  # URL /admins/sign_in ...
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
