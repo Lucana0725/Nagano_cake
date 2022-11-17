69b6066e1d394b918e242474bb0a47a0b5541b95
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-17 14:23:33 +0000

    [Fixed]AddressモデルとGenreモデルのカラムにNOT NULLを設定

--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,10 @@ ActiveRecord::Schema.define(version: 2022_11_17_140955) do
   end
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "postal_code"
-    t.string "address"
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2022_11_17_140955) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
