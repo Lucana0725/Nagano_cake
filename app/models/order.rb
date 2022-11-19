355f9dfcd54e6b182e24ad15516a7400edb420bd
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-19 06:47:24 +0000

    [Finish]Orderモデルに受注ステータスのenumを定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,5 @@ class Order < ApplicationRecord
   
   # enumを使って支払い方法を分岐。(0: クレカ、1:銀行振り込み)
   enum payment_method: { credit_card: 0, transfer: 1 }
-  # enum status: { }
+  enum order_status: { waiting_payment: 0, checked_payment: 1, creating: 2, ready_to_send: 3, finished_send: 4 }
 end
