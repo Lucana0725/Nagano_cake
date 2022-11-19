3bf71abd5301e242a7af1cbd1f0a8ea8f0be3ec6
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-19 12:28:05 +0000

    [Finish]admin/genre#indexの追記、#createの作成、ストロングパラメーターの作成、viewの追記

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,13 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
-    # @genres = Genre.all
+    @genres = Genre.all
   end
 
   def create
-    # @genre = Genre.new(genre_params)
-    # @genre.save
-    # redirect_to admin_genres_path
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
@@ -17,9 +17,10 @@ class Admin::GenresController < ApplicationController
   end
   
   
-  # private
+  private
   
-  # def genre_params
-  #   params.require(:genre).permit(:name)
-  # end
+  # ストロングパラメーター
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
