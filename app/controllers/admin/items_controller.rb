38efacf8e3ecd8eb0616b2d5de08b1db55b9febc
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-19 14:35:34 +0000

    [Finish]商品登録画面の作成、new, create, indexアクションの作成、テストで一つデータを追加済、次はindexページを作成する

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,16 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to (admin_items_path)
   end
 
   def show
