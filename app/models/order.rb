dfe0207c91cb2f2aacebfedf3610d584bf23fad9
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-19 06:25:57 +0000

    [Fixed]enumを定義する先を間違えていたので修正(order_detail.rb => order.rb)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+  
+  # enumを使って支払い方法を分岐。(0: クレカ、1:銀行振り込み)
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
