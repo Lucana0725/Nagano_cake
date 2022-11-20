3bdcfcb9d0b888c5136fe891a965c96ed6d757f8
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-11-20 12:49:24 +0000

    [Fixed]チャレンジ項目のジャンル関係を一旦削除、アソシエーションも削除、とりあえず投稿ができるように変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,8 +9,10 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to (admin_items_path)
+    # binding.pry
+    @item.save!
+    # redirect_to (admin_items_path)
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -22,10 +24,10 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
-  
-  
+
+
   private
-  
+
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :image)
   end
