68a452a8ab85a903e85635970c1b6dcd0f2c9d92
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-12-04 13:22:35 +0000

    [Fixed]カート内情報を表示できるよう改善

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -13,9 +13,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    # Customerとアソシエーションを結んでいるので親になるCustomerの情報(id)が必要になる
+    # @cart_item = current_customer.cart_items.new(cart_item_params)  下二行と同じ意味合い。「現在のcustomerのcart_items（Customerモデルでアソシエーションを結んだ際の"has_many :cart_items"をそのまま持ってきて使用）を新しく作りなさいよ」という記述。
     @cart_item = CartItem.new(cart_item_params)
-    binding.pry
-    @cart_item.save
+    @cart_item.customer_id = current_customer.id
+
+
+    # binding.pry
+
+    @cart_item.save!
     redirect_to cart_items_path
   end
 
