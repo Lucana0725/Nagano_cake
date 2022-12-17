300214bc4d811918dea0298534455cd2dbd75d61
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-12-17 14:20:24 +0000

    [Fixed]new->confirmへの遷移をget->postに変更、ルーティングも同様に変更

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Rails.application.routes.draw do
   end
   
   scope module: :public do
-    get 'orders/confirm' => 'orders#confirm', as: "orders_confirm"
+    post 'orders/confirm' => 'orders#confirm', as: "orders_confirm"
     get 'orders/finish' => 'orders#finish', as: "orders_finish"
     get 'orders/show' => 'orders#show', as: "orders_show"
     resources :orders, only:[:new, :create, :index]
