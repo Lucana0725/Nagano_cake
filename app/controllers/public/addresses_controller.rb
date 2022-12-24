d30c2f7fd3f616151d1eb1ddcfa5f373ee2db8e6
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-12-24 14:20:55 +0000

    [Finish]配送先の追加機能を実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,32 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @addresses = Address.all
+    # @addresses = Address.all
+    @addresses = current_customer.addresses
   end
 
   def edit
   end
 
   def create
+    # @address.customer.id = current_customer.id
+    # @address = current_customer.address
+    # byebug
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id  #byebugでデバッグしたら@address.saveの直前のところで"@address"とすると"customer_id: nill"と出たのでおそらく必要。 書く順番に注意。(@address.newの前に)
+    @address.save!
+    redirect_to addresses_path
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
