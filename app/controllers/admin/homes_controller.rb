18cfb46c9ab28d6eadb31fd0610661074e314f85
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-12-27 12:29:49 +0000

    [Finish]注文個数の表示の実装

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,9 @@
 class Admin::HomesController < ApplicationController
   def top
-    
+    # @order_details = OrderDetail.all
+    @orders = Order.all  # 注文の履歴を一覧で出したいのでOrderモデルから全てを引っ張ってくる
+    # byebug
+    # @order.amount = order_details.amount
+    # @ordered_total_amount = 0  # 注文合計の定義や計算はviewの方でやった方がわかりやすいので今回はこちらは却下。
   end
 end
