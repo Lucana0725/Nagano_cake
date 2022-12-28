859f4dec8f67518b7554a647d1f126e63b1d535b
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-12-28 15:21:35 +0000

    [Finish]製作ステータス以外の表示を実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details  # showページ下側のtableで注文内容(OrderDetail)を取り出すために全てのOrderDetailを取得する必要がある。そしてそれは単一のOrderのページにて表示されるので上で定義の@orderにアソシエーションで紐づいている.order_detailsとなる。
   end
   
   def update
+    # byebug
     @order = Order.find(params[:id])
     @order.update(order_status_params)
     redirect_to admin_order_path
