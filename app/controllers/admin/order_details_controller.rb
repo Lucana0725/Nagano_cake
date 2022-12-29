e26e4029b5e8ab125201ea42b023d2994cb7f99e
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-12-29 12:38:29 +0000

    [Finish]create_adminで作成、マージできたはずなのになぜか残っているのでとりあえずcommit

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,13 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   # 提出上、最悪実装の必要はない。
-  
+
   def update
     # byebug
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(create_status_params)
     redirect_to admin_order_path
   end
-  
+
   private
   def create_status_params
     params.require(:order_detail).permit(:create_status)
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details  # showページ下側のtableで注文内容(OrderDetail)を取り出すために全てのOrderDetailを取得する必要がある。そしてそれは単一のOrderのページにて表示されるので上で定義の@orderにアソシエーションで紐づいている.order_detailsとなる。
   end
-  
+
   def update
     # byebug
     @order = Order.find(params[:id])
@@ -14,12 +14,12 @@ class Admin::OrdersController < ApplicationController
     # end
     redirect_to admin_order_path
   end
-  
+
   private
   def order_status_params
     params.require(:order).permit(:order_status)
   end
-  
+
   # def create_status_params
   #   params.require(:order_detail).permit(:create_status)
   # end
